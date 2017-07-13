9288772c9a5564405a95848a9ed342f07447ebe1
Aaron <amiller719@gmail.com>
2017-07-12 20:27:49 -0400

    Changing from local database to mlab

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const DATABASE_URL = process.env.DATABASE_URL ||
                      global.DATABASE_URL ||
-                     'mongodb://localhost/mini-blog-app';
-const PORT = process.env.PORT || 1337;
+                     'mongodb://a:a@ds153392.mlab.com:53392/mini-blog-app';
+const PORT = process.env.PORT || 8080;
 const TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
                           global.DATABASE_URL ||
-                          'mongodb://localhost/test-database';
+                          'mongodb://a:a@ds157282.mlab.com:57282/mini-blog-app-test';
 module.exports = {
   DATABASE_URL,
   TEST_DATABASE_URL,
